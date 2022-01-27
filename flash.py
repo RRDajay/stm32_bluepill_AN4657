92fa58d6e1a122dfd2cbe4749f05a87ada962976
RRDajay <ryan.rdajay@gmail.com>
2022-01-27 09:27:40 +0800

    added py script to calculate for flash page boundaries

--- flash.py
+++ flash.py
@@ -3,5 +3,5 @@ flash_page_size = 1023
 for num in range(0,128):
     start = hex(0x8000000 + (flash_page_size*num) + num)
     end = hex(0x8000000 + (flash_page_size*(num+1)) + num)
-    print("{}".format(start))
+    print("Page {}: {} {}".format(num, start, end))
 
